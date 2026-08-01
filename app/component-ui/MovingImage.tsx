1b0c54d2d6188d87327e1c03009ae500a137c93f
Zyrus Hiyao <zyrushiyao@gmail.com>
2026-08-01 19:47:27 +0800

    Animation polish

--- app/component-ui/MovingImage.tsx
+++ app/component-ui/MovingImage.tsx
@@ -13,6 +13,9 @@ export default function MovingImage() {
 
   useGSAP(
     () => {
+      //shadow init
+      gsap.set("#shadow", { opacity: 0, scaleX: 0.0 });
+
       gsap
         .timeline({
           id: "herman-tl",
@@ -23,9 +26,7 @@ export default function MovingImage() {
             duration: 0.5,
           },
         })
-        //shadow init
-        .set("#shadow", { scaleX: 0 })
-        // .set("#herman-wrapper", { y: 0, scaleX: 1, scaleY: 1 })
+
         // hole appears close to open
         .from("#hole", { scale: 0, duration: 1.5 })
         // herman jump
@@ -42,37 +43,46 @@ export default function MovingImage() {
             delay: 0.1,
             scale: 1,
             y: -220,
-            duration: 1.3,
+            duration: 0.7,
             ease: "power1.in",
           },
           "-=0.1",
         )
         // hole disappears
-        .to("#hole", { scale: 0, duration: 0.7, ease: "power1.out" }, "<")
+        .to("#hole", { scale: 0, duration: 0.5, ease: "power1.out" }, "<")
         // set shadow and expand a bit when herman is about to land
-        .set("#shadow", { scaleX: 0.2 }, "<")
-        .to("#shadow", { scaleX: 0.6, duration: 0.6, ease: "linear" }, "-=0.7")
+        .to(
+          "#shadow",
+          { opacity: 0.6, scaleX: 0.5, duration: 0.5, ease: "slow" },
+          "-=0.5",
+        )
         //   herman squashes
         .to(
           "#herman-wrapper",
           {
             transformOrigin: "center bottom",
             scaleY: 0.6,
-            scaleX: 1.5,
-            ease: "power1.inOut",
-            duration: 0.5,
+            scaleX: 1.3,
+            ease: "power1.in",
+            duration: 0.3,
           },
           "-=0.1",
         )
         //shadow expands when Herman squish
-        .to("#shadow", { scaleX: 1, duration: 1.3, ease: "back(1.7)" }, "<")
         .to(
-          "#herman-wrapper",
-          { scale: 1, duration: 1, ease: "back(1.7)" },
-          //   "-=0.3",
+          "#shadow",
+          {
+            opacity: 1,
+            scaleX: 0.7,
+            duration: 0.3,
+            ease: "slow",
+          },
+          "-=0.2",
         )
+
+        .to("#herman-wrapper", { scale: 1, duration: 1, ease: "back(1.7)" })
         // shadow shrinks
-        .to("#shadow", { scaleX: 0.7, duration: 0.7, ease: "power1" }, "<");
+        .to("#shadow", { scaleX: 0.5, duration: 1, ease: "power1" }, "<");
 
       GSDevTools.create({
         id: "herman-devtools",
@@ -95,7 +105,7 @@ export default function MovingImage() {
         >
           <div
             id="hole"
-            className="absolute top-95 left-1/2 -translate-1/2 border-2 border-black rounded-[50%] h-5 w-60 bg-black  "
+            className="absolute top-95 left-1/2 -translate-1/2 border-2 border-black rounded-[50%] h-10 w-60 bg-black  "
           ></div>
 
           <div
