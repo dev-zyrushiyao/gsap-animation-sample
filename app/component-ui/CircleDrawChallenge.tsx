799eea6034da62029a17c6b4df9a23b467d53a5d
Zyrus Hiyao <zyrushiyao@gmail.com>
2026-09-06 21:21:38 +0800

    Added ScrollTrigger for FlowerSVG and created a CircleDrawChallenge

---
+++ app/component-ui/CircleDrawChallenge.tsx
@@ -0,0 +1,122 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import DrawSVGPlugin from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+import React, { useRef } from "react";
+
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+
+export default function CircleDrawChallenge() {
+  const container = useRef(null);
+
+  useGSAP(
+    () => {
+      const circleA = document.querySelector(".circle-A");
+      const lineConnector = document.querySelector(".line-connector");
+      const circleB = document.querySelector(".circle-B");
+
+      gsap.set(circleA, { rotate: -90, transformOrigin: "50% 50%" });
+      gsap.set(circleB, { rotate: -90, transformOrigin: "50% 50%" });
+
+      const tl = gsap
+        .timeline()
+        .fromTo(circleA, { drawSVG: "100% 100%" }, { drawSVG: "150% 50%" })
+        .from(lineConnector, { drawSVG: 0 })
+        .fromTo(circleB, { drawSVG: "100% 100%" }, { drawSVG: "150% 50%" });
+
+      GSDevTools.create({ animation: tl });
+    },
+    { scope: container },
+  );
+
+  return (
+    <div ref={container}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 400 500"
+        width={500}
+        height={500}
+      >
+        <defs>
+          <linearGradient
+            gradientUnits="userSpaceOnUse"
+            x1={490.143}
+            y1={49.982}
+            x2={490.143}
+            y2={568.899}
+            id="red-blue-circle-gradient"
+            gradientTransform="matrix(1, 0, 0, 0.963546, -290.143744, -48.159653)"
+          >
+            <stop
+              offset={0}
+              style={{
+                stopColor: "rgb(79, 147, 216)",
+              }}
+            />
+            <stop
+              offset={0.473}
+              style={{
+                stopColor: "rgb(91, 90, 137)",
+              }}
+            />
+            <stop
+              offset={1}
+              style={{
+                stopColor: "rgb(129, 46, 56)",
+              }}
+            />
+          </linearGradient>
+          <mask id="red-blue-circle-mask">
+            <g transform="matrix(1, 0, 0, 1, 0.262001, -50.000009)">
+              <g>
+                <polyline
+                  style={{
+                    fill: "none",
+                    strokeWidth: 20,
+                    stroke: "white",
+                  }}
+                  points="200.07 200 200 400"
+                  className="line-connector"
+                />
+                <ellipse
+                  style={{
+                    fill: "none",
+                    stroke: "white",
+                    strokeWidth: 13,
+                  }}
+                  cx={199.419}
+                  cy={150.227}
+                  rx={49.773}
+                  ry={49.773}
+                  className="circle-A"
+                />
+                <ellipse
+                  style={{
+                    fill: "none",
+                    stroke: "white",
+                    strokeWidth: 13,
+                  }}
+                  cx={200.057}
+                  cy={449.773}
+                  rx={49.773}
+                  ry={49.773}
+                  className="circle-B"
+                />
+              </g>
+            </g>
+          </mask>
+        </defs>
+        <rect
+          x={79.393}
+          width={241.215}
+          height={500}
+          style={{
+            stroke: "rgb(0, 0, 0)",
+            fill: "url(#red-blue-circle-gradient)",
+            mask: "url(#red-blue-circle-mask)",
+          }}
+        />
+      </svg>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -40,6 +40,7 @@ import ShapeMorp from "./component-ui/ShapeMorp";
 import MorphPageTransition from "./component-ui/MorphPageTransition";
 import BlendingBanner from "./component-ui/BlendingBanner";
 import FlowerSVG from "./component-ui/FlowerSVG";
+import CircleDrawChallenge from "./component-ui/CircleDrawChallenge";
 
 gsap.registerPlugin(useGSAP);
 
@@ -67,7 +68,7 @@ export default function Home() {
 
   return (
     <main ref={container}>
-      <div className="flex justify-center bg-lime-600">
+      {/* <div className="flex justify-center bg-lime-600">
         <div className="title-container bg-amber-300 w-fit h-40 flex flex-row text-5xl justify-center items-center p-5">
           <p className="font-serif name-title relative ">Z</p>
           <p className="font-serif name-title relative ">Y</p>
@@ -112,7 +113,8 @@ export default function Home() {
       <ShapeMorp />
       <MorphPageTransition />
       <BlendingBanner />
-      <FlowerSVG />
+      <FlowerSVG /> */}
+      <CircleDrawChallenge />
     </main>
   );
 }
